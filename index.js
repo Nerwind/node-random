2d7360b7af53eb3659236d5a21d0e9db2ee1fc27
nerwind93 <nerwindgd@gmail.com>
2021-04-15 21:41:25 +0500

    node-random

---
+++ index.js
@@ -0,0 +1,37 @@
+class Random {
+    /**
+     * 
+     * @param {Number} min 
+     * @param {Number} max 
+     */
+    static int(min, max) {
+        return !max ? Math.floor(Math.random() * min) : Math.floor(Math.random() * max) + min;
+    }
+
+    /**
+     * @param {Array} list 
+     */
+
+    static choice(list) {
+        return !list[0] ? null : list[this.int(list.length)];
+    }
+
+    /**
+     * @param {Array} list 
+     */
+
+    static shuffle(list) {
+        let ar = [],
+        b = list.length;
+
+        for (let a = 0; a < b;) {
+            let r = Math.floor(Math.random() * list.length);
+            ar.push(list[r]); list.splice(r, 1);
+            b = list.length;
+        }
+
+        return ar;
+    }
+}
+
+module.exports = Random;
